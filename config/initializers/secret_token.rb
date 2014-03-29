1b553ea136c39cda415a585c2bd41b7feef032b5
Scott Willson <scott.willson@gmail.com>
2014-03-28 18:27:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Racing::Application.config.secret_token = '2c7548934971498ae004cfb99ca333caa6331efd889f4dc7e4585eaf6ad166eaf440c5988afafb74363c8836369f9c80eec684307e83bbd8c88de10eda9e0dd4'
