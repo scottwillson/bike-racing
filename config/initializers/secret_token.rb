c1c6024d95e87ac28b236128c9de45359d143bad
Scott Willson <scott.willson@gmail.com>
2014-03-28 18:39:04 -0700

    Rails 4 not 3

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Racing::Application.config.secret_token = '2c7548934971498ae004cfb99ca333caa6331efd889f4dc7e4585eaf6ad166eaf440c5988afafb74363c8836369f9c80eec684307e83bbd8c88de10eda9e0dd4'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Racing::Application.config.secret_key_base = '2734c86c93e2fc326995ea5c522ca479e750c19062e62a8ad3fa6f225d47adac2fae8f6fde210309a193836612d184713ca1bf52597be89c528c3546a4e08bcf'
